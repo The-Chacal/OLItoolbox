4b0d2cd5cd0f9dfbee49280972e4c91bfda0a46e
Chacal <sylvainlorent@gmail.com>
2024-09-12 11:17:19 +0200

    Release

--- OLItoolbox.jsx
+++ OLItoolbox.jsx
@@ -8,7 +8,7 @@ creatingUI( this );
  */
 function creatingUI( thisObj ){
     
-    var versionNb = "1.1.2" ;
+    var versionNb = "1.1.2" ;//Released
     var OLItoolboxDlg = thisObj ;
         var OLItoolboxdlgGroup = OLItoolboxDlg.add( "Group" );
             OLItoolboxdlgGroup.orientation = "Column" ;
